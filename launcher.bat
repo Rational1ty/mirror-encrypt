bc1bc0c7ba582f278cab1b5ef6e3ecc8c0d1d830
Matthew Davidson <mdavidson390@outlook.com>
2020-08-26 23:32:32 -0500

    launcher done for now

--- launcher.bat
+++ launcher.bat
@@ -15,21 +15,20 @@ cd %~dp0
 if not exist ./bin mkdir bin
 
 :: Compile the program (lib files first, then src files)
-cd lib
-javac -d ../bin -cp ../bin *.java
-if errorlevel 1 goto:fatal
-
-cd ../src
+cd lib && ^
+javac -d ../bin -cp ../bin *.java && ^
+cd ../src && ^
 javac -d ../bin -cp ../bin *.java
 if errorlevel 1 goto:fatal
 
 :: Run the program
 cd ../bin
 java src.MirrorEncrypt %commands%
+if errorlevel 1 goto:fatal
 
 cmd /k
 goto:eof
 
 :fatal
-echo "A fatal error occurred while launching"
-cmd /k
+echo A fatal error occurred while launching
+pause
