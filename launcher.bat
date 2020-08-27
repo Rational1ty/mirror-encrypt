15930358323c9cd25634ed87571dba49026e6367
Matthew Davidson <mdavidson390@outlook.com>
2020-08-26 23:14:23 -0500

    Updated launcher - Now checks if bin exists before compiling; if bin is missing, it is created - Added error checking after compiling lib and src - Added error message if compilation fails

--- launcher.bat
+++ launcher.bat
@@ -12,16 +12,24 @@ echo Launching MirrorEncrypt.java...
 cd %~dp0
 
 :: Create bin
-mkdir bin 2>&1 nul
+if not exist ./bin mkdir bin
 
 :: Compile the program (lib files first, then src files)
 cd lib
 javac -d ../bin -cp ../bin *.java
+if errorlevel 1 goto:fatal
+
 cd ../src
 javac -d ../bin -cp ../bin *.java
+if errorlevel 1 goto:fatal
 
 :: Run the program
 cd ../bin
 java src.MirrorEncrypt %commands%
 
+cmd /k
+goto:eof
+
+:fatal
+echo "A fatal error occurred while launching"
 cmd /k
