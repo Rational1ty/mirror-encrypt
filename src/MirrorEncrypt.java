f15478d90aeadf7f75178ddd84d989baa7e6d60f
Matthew Davidson <mdavidson390@outlook.com>
2020-08-30 22:49:32 -0500

    Added comments

--- src/MirrorEncrypt.java
+++ src/MirrorEncrypt.java
@@ -24,6 +24,7 @@ public class MirrorEncrypt {
 
         displayMainMenu();
 
+        // Main menu loop
         while (choice != 0) {
             do {
                 out.print("\nEnter choice: ");
