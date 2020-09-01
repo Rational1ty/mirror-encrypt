f5a2204874b88e00a58da65e6e33029d0701fe0f
Matthew Davidson <mdavidson390@outlook.com>
2020-09-01 13:52:02 -0500

    Changes to toMirrorField method - Added empty input checking - Removed 'n\N' feature; use 12 space (number 12) as equivalant - fixed several bugs which would cause crashes - Improved documentation

--- src/MirrorEncrypt.java
+++ src/MirrorEncrypt.java
@@ -209,61 +209,59 @@ public class MirrorEncrypt {
     // If [mirrors] contains any integers, they will be parsed as the respective number of spaces
     // If [mirrors] contains the letter n, it will be parsed as a newline character
     static char[] toMirrorField(String mirrors) throws IllegalArgumentException {
-        //char[] field = new char[13 * 13];
-        final int flen = 13 * 13;
-        String fstr = "";
+        final int flen = 169;
+
+        if (mirrors.equals("")) {
+            return new char[flen];
+        }
+
+        String field = "";
 
         // Check for any invalid characters
+        // Accepted characters are: /, \, 0-9, n, and space
         if (!mirrors.matches("[\\\\/0-9n\\s]*"))
             throw new IllegalArgumentException("Invalid character entered; accepted characters are /, \\, 0-9, n, or space");
 
+        // Put mirrors into field
         if (MirrorConstants.REPEAT_SEQ) {
-            for (int i = 0; i < flen; i += mirrors.length())
-                mirrors += mirrors;
+            // Repeat the sequence until field is full
+            // Always repeat until field length > flen, then trim so that field length = flen
+            field = mirrors.repeat( (int) Math.ceil( (double) flen / mirrors.length() ) );
+            field = field.substring(0, flen);
         } else {
-            for (int i = mirrors.length(); i < flen; i++)
-                mirrors += " ";
+            // Otherwise, fill the rest of field with spaces
+            field = mirrors += " ".repeat(flen - mirrors.length());
         }
 
-        // If user entered any numbers in the [mirrors], convert them to spaces
+        // If user entered any numbers in mirrors, convert them to spaces
         // Ex: /5/ becomes /     /
-        // Also, if user entered the letter 'n' convert it to a newline character
-        int pos = 0;
-        for (char c : mirrors.toCharArray()) {
-            if (c == '/' || c == '\\' || c == ' ') {
-                fstr += c;
-                pos++;
+        String r = "";
+        String spaces = "";
+        for (int i = 0; i < field.length(); i++) {
+            char c = field.charAt(i);
+
+            // numbers 0-9
+            if ((c + "").matches("[0-9]")) {
+                spaces += c;
                 continue;
             }
 
-            if (c == 'n') {
-                int dist = 13 - (pos % 13);
-                while (dist > 0) {
-                    fstr += " ";
-                    pos++;
-                    dist--;
-                }
-                continue;
+            // anything other than a number; check if spaces need to be inserted first
+            if (spaces.length() > 0) {
+                int buffer = Integer.parseInt(spaces);
+                r += " ".repeat(buffer);
+                spaces = "";
             }
 
-            try {
-                int spaces = Integer.parseInt(c + "");
-                while (spaces > 0) {
-                    fstr += " ";
-                    pos++;
-                    spaces--;
-                }
-            } catch (NumberFormatException ex) {
-                out.println("Fatal internal error: " + ex);
-                System.exit(-1);
-            }
+            // space, /, or \
+            r += c;
         }
         
-        // Trim fstr to the correct length if it's too long
-        if (fstr.length() > flen) {
-            fstr = fstr.substring(0, flen + 1);
+        // Trim r to the correct length if it's too long
+        if (r.length() > flen) {
+            r = r.substring(0, flen);
         }
         
-        return fstr.toCharArray();
+        return r.toCharArray();
     }
 }
